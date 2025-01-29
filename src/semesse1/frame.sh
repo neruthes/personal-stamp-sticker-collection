f4ef5f84793a2dbb2a562bb338cd54d500202d03
Neruthes <i@neruthes.xyz>
2025-01-29 22:13:56 +0000

    Regular update '2025-01-29T22:13:56+00:00'

--- src/semesse1/frame.sh
+++ src/semesse1/frame.sh
@@ -1,6 +1,8 @@
 cd src/semesse1 || exit 1
 mkdir -p frame
 
+magick semesse1.js.png -crop 2909x2000+0+0 frame/semesse1.rz5533-item.png
+
 magick semesse1.js.png -crop 2909x2000+0+0 -bordercolor white -border 364 frame/semesse1.15x20.png
 magick semesse1.js.png -crop 2667x2000+0+0 -bordercolor white -border 333 frame/semesse1.20x30.png
 magick semesse1.js.png -crop 3000x2000+0+0 -bordercolor white -border 250 frame/semesse1.25x35.png
