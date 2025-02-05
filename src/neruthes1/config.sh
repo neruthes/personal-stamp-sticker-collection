0505ccf6bfa041868c8581732d6741ae1471854b
Neruthes <i@neruthes.xyz>
2025-02-05 04:13:20 +0000

    Update neruthes1

--- src/neruthes1/config.sh
+++ src/neruthes1/config.sh
@@ -1,3 +1,20 @@
 IM_ANCHOR=SouthWest
 is_landscape=y
-rz_hover_path=misc/deco-star1-gold.png
+rz_hover_path=src/neruthes1/extra/surface-silver.png
+
+
+
+### When executing this hook, PWD is ./src/proj_id
+function _hook_preframing() {
+    rsvg-convert ./extra/surface.svg -o ./extra/surface.png; realpath ./extra/surface.png
+    rsvg-convert <(sed 's|black|#F1D877|g' ./extra/surface.svg) -o ./extra/surface-gold.png
+    rsvg-convert <(sed 's|black|#DDDDDD|g' ./extra/surface.svg) -o ./extra/surface-silver.png
+    rsvg-convert ./extra/avatar.svg --width 1650 -o ./extra/avatar.png
+    magick composite ./extra/avatar.png ./neruthes1.js.png ./neruthes1.js.png
+}
+
+
+function _preparation_notes() {
+    # Run only once when preparing files into repo. Do not execute in cooking.
+    printf ''
+}
--- src/neruthes1/neruthes1.js
+++ src/neruthes1/neruthes1.js
@@ -3,10 +3,10 @@
 
 let OUTPUT_TEMPLATE = `<svg viewBox="0 0 2000 1000" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="-100" y="-100" width="2200" height="1200" fill="#ffffff"  />
-    <path d="M -300 150 l 2700 700 l 0 3000 l -2700 0 z" stroke="#453613" stroke-width="380" fill="#fecf58" />
+    <path d="M -300 150 l 2700 700 l 0 3000 l -2700 0 z" stroke="#453613" stroke-width="380" fill="#fecf58" opacity="0" />
 
-    <text font-family="Myriad Pro" font-weight="400" font-size="250" x="1" y="985"
-        fill="#453613" stroke="#453613" stroke-width="1" transform="scale(0.85,1)">NERUTHES</text>
+    <text font-family="New Heterodox Mono" font-weight="400" font-size="250" x="1" y="985" opacity="0"
+        fill="#453613" stroke="#453613" stroke-width="1" transform="scale(1,1)">NERUTHES</text>
 </svg>
 `;
 
